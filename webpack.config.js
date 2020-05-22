acd598fba99a5259349499261d9411ea1bef2782
ma-ya's create <hello.myscreate@gmail.com>
2020-05-22 22:39:41 +0900

    install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,15 @@ const path = require('path');
 const outputPath = path.resolve(__dirname, 'dist')
 
 module.exports = {
+	//バンドルするファイル（エントリーポイント）
 	entry: './src/index.js',
+	//コンパイル先
 	output: {
 		filename: 'main.js',
 		path: outputPath
+	},
+	//デフォルトで開く仮想サーバーのディレクトリ
+	devServer: {
+		contentBase: outputPath
 	}
 }
