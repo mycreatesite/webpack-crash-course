0e5c0d4a5a72623c2d6d2aef34a132d570f948b4
ma-ya's create <hello.myscreate@gmail.com>
2020-05-23 14:51:39 +0900

    install optimize-css-assets-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'dist');
 
@@ -66,7 +67,8 @@ module.exports = {
 						drop_console: true
 					}
 				}
-			})
+			}),
+			new OptimizeCSSAssetsPlugin({})
 		]
 	}
 }
